92757239dce288162e92d49b72361e83d7527ff4
Hasan Mahmud <hasan.mahmud@accenture.com>
2023-04-14 01:35:41 +0200

    data class added

--- _6_OOPLearn/src/main/kotlin/Main.kt
+++ _6_OOPLearn/src/main/kotlin/Main.kt
@@ -1,10 +1,23 @@
 fun main(args: Array<String>) {
     //Test Nested Class
-    nestedClassLearn()
+    //nestedClassLearn()
 
     //Test Constructor
-    constructorLearn()
+    //constructorLearn()
 
     //Test interface and inheritence
-    interfaceLearn()
+    //interfaceLearn()
+
+    //Data class
+    data class man(val name: String, val age: Int)
+    {
+        var height: Int = 0;
+    }
+    val man1 = man("manish",18)
+    val man2 = man1.copy(name="rahul") //copying details of man1 with change in name of man
+    man1.height=100
+    man2.height=90
+    println("${man1} has ${man1.height} cm height")
+    println("${man2} has ${man2.height} cm height")
+
 }
