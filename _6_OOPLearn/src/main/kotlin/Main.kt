87c6b515ca0b332f8d920c81f63ea39de94af001
Hasan Mahmud <hasan.mahmud@accenture.com>
2023-04-14 01:11:43 +0200

    interface, inheritence learning added

--- _6_OOPLearn/src/main/kotlin/Main.kt
+++ _6_OOPLearn/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
     //Test Nested Class
-    //nestedClassLearn()
+    nestedClassLearn()
 
     //Test Constructor
     constructorLearn()
+
+    //Test interface and inheritence
+    interfaceLearn()
 }
